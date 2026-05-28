6e5bd47de5ccc5aecfd484d34fd8374247d7729d
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-28 19:10:48 +0600

    Solved cors issue

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(helmet());
 
 app.use("/api/auth", authRoutes);
